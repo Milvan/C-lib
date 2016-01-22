fd679ed6a0139010146df3eccd8b6505c48702b1
Marcus <mlar5@kth.se>
2016-01-22 13:55:03 +0100

    Initial commit. A double linked list with stack and queue support

---
+++ linkedList/linkedList.h
@@ -0,0 +1,31 @@
+#ifndef __LinkedList__
+#define __LinkedList__
+#endif
+
+#ifdef DEBUG
+#define dprint(x) (fprintf(stderr, "DEBUG: %s",x))
+#else
+#define dprint(x)
+#endif
+
+typedef void *value;
+
+typedef struct node{
+	value *x;
+	struct node *next;
+	struct node *prev;
+} Node;
+
+typedef struct {
+	struct node *first;
+	struct node *last;
+	int length;
+} LinkedList;
+
+void push(LinkedList *list, value *val);
+Node *pop(LinkedList *list);
+
+void queue(LinkedList *list, value *val);
+Node *dequeue(LinkedList *list);
+
+int removeElement(LinkedList *list, Node *node);
